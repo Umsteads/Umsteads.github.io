9f1144c9df0e6a1be4dc80ddea2e65570ff134f7
Umsteads <71802534+Umsteads@users.noreply.github.com>
2020-09-25 12:05:41 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,68 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Recaptcha Demo</title>
+<script src='https://www.google.com/recaptcha/api.js'></script>
+</head>
+<body> 
+
 <?php
-// set post fields
-$post = [
-    'secret' => "6Lcl8MYZAAAAAIJLgXvCyVOAIpf86orARsZCmtG7",
-    'response' => $_POST['g-recaptcha-response'],
-    'remoteip'   => $_SERVER['REMOTE_ADDR']
-];
+// Checks if form has been submitted
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    function post_captcha($user_response) {
+        $fields_string = '';
+        $fields = array(
+            'secret' => '6LcDdNAZAAAAAJFjR6cUWc60R5gpcK13OC2OFnVP',
+            'response' => $user_response
+        );
+        foreach($fields as $key=>$value)
+        $fields_string .= $key . '=' . $value . '&';
+        $fields_string = rtrim($fields_string, '&');
 
-$ch = curl_init('https://www.google.com/recaptcha/api/siteverify');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.google.com/recaptcha/api/siteverify');
+        curl_setopt($ch, CURLOPT_POST, count($fields));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
 
-// execute!
-$response = curl_exec($ch);
+        $result = curl_exec($ch);
+        curl_close($ch);
 
-// close the connection, release resources used
-curl_close($ch);
+        return json_decode($result, true);
+    }
 
-// do anything you want with your response
-var_dump(json_decode($response));
-?>
+    // Call the function post_captcha
+    $res = post_captcha($_POST['g-recaptcha-response']);
+
+    if (!$res['success']) {
+        // What happens when the CAPTCHA wasn't checked
+        echo '<p>Please go back and make sure you check the security CAPTCHA box.</p><br>';
+    } else {
+        // If CAPTCHA is successfully completed...
+
+        // Paste mail function or whatever else you want to happen here!
+        echo '<br><p>CAPTCHA was completed successfully!</p><br>';
+    }
+} else { ?>
+    
+    <!-- FORM GOES HERE -->
+    <form action="recaptcha-v2.php" method="post">
+        <label for="fname">First Name*</label><br>
+        <input type="text" name="fname" id="fname" required autofocus><br><br>
+
+        <label for="lname">Last Name*</label><br>
+        <input type="text" name="lname" id="lname" required><br><br>
+
+        <label for="email">Email Address*</label><br>
+        <input type="email" name="email" id="email" required><br><br>
+
+        <div class="g-recaptcha" data-sitekey="6LcDdNAZAAAAAEqfKuFfYKBF0Y7wPQ_NdPBnNl6x"></div>
+        <br>
+        <input type="submit" id="submit" value="Submit">
+    </form>
+
+<?php } ?>
+
+</body>
+</html>
