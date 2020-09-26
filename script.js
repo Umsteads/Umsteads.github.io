784daa89e24b4cb8e3c51da0691137fa1bf5fc09
Umsteads <71802534+Umsteads@users.noreply.github.com>
2020-09-26 13:19:50 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("my_captcha_form").addEventListener("submit", function (evt) {
+
+    var response = grecaptcha.getResponse();
+    if (response.length == 0) {
+        //reCaptcha not verified
+        alert("please verify you are humann!");
+        evt.preventDefault();
+        return false;
+    }
+    console.log(response)
+    //captcha verified
+    //do the rest of your validations here
+
+});
